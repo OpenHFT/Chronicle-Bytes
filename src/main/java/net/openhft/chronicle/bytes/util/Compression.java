bbd05dd842a2318340bddfce2366a9ead3be2e5e
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-12-10 14:02:41 +0000

    Add LZW Compression

---
+++ src/main/java/net/openhft/chronicle/bytes/util/Compression.java
@@ -0,0 +1,7 @@
+package net.openhft.chronicle.bytes.util;
+
+/**
+ * Created by peter.lawrey on 09/12/2015.
+ */
+public class Compression {
+}
---
+++ src/test/java/net/openhft/chronicle/bytes/util/LZWTest.java
@@ -0,0 +1,13 @@
+package net.openhft.chronicle.bytes.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by peter.lawrey on 09/12/2015.
+ */
+public class LZWTest extends TestCase {
+
+    public void testCompress() throws Exception {
+
+    }
+}
