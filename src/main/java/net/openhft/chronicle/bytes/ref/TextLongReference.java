b78a1c01d9f214f0c42041f68fdaa5f25a80a6cd
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-03-23 11:36:39 +0000

    Code cleanup

--- src/main/java/net/openhft/chronicle/bytes/ref/TextLongReference.java
+++ src/main/java/net/openhft/chronicle/bytes/ref/TextLongReference.java
@@ -1,17 +1,19 @@
 /*
- *     Copyright (C) 2015  higherfrequencytrading.com
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License.
+ *  *     Copyright (C) ${YEAR}  higherfrequencytrading.com
+ *  *
+ *  *     This program is free software: you can redistribute it and/or modify
+ *  *     it under the terms of the GNU Lesser General Public License as published by
+ *  *     the Free Software Foundation, either version 3 of the License.
+ *  *
+ *  *     This program is distributed in the hope that it will be useful,
+ *  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *     GNU Lesser General Public License for more details.
+ *  *
+ *  *     You should have received a copy of the GNU Lesser General Public License
+ *  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.openhft.chronicle.bytes.ref;
 
@@ -66,7 +68,6 @@ public class TextLongReference implements LongReference {
             if (this.bytes != null)
                 this.bytes.release();
             bytes.reserve();
-            ;
         }
         this.bytes = bytes;
         this.offset = offset;
