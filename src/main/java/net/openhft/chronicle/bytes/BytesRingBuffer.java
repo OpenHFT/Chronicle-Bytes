8f4cc044c37efe97dfc87fc72fe031cd7f21ebfe
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-03-23 08:52:41 +0000

    Add copyright

--- src/main/java/net/openhft/chronicle/bytes/BytesRingBuffer.java
+++ src/main/java/net/openhft/chronicle/bytes/BytesRingBuffer.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  *     Copyright (C) ${YEAR}  higherfrequencytrading.com
+ *  *
+ *  *     This program is free software: you can redistribute it and/or modify
+ *  *     it under the terms of the GNU Lesser General Public License as published by
+ *  *     the Free Software Foundation, either version 3 of the License.
+ *  *
+ *  *     This program is distributed in the hope that it will be useful,
+ *  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *     GNU Lesser General Public License for more details.
+ *  *
+ *  *     You should have received a copy of the GNU Lesser General Public License
+ *  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package net.openhft.chronicle.bytes;
 
 import net.openhft.chronicle.core.Jvm;
@@ -14,32 +32,6 @@ import java.lang.reflect.Method;
 public interface BytesRingBuffer extends BytesRingBufferStats, BytesConsumer {
 
     Logger LOG = LoggerFactory.getLogger(BytesRingBuffer.class);
-
-    /**
-     * clears the ring buffer but moving the read position to the write position
-     */
-    void clear();
-
-
-    /**
-     * Inserts the specified element at the tail of this queue if it is possible to do so
-     * immediately without exceeding the queue's capacity,
-     *
-     * @param bytes0 the {@code bytes0} that you wish to add to the ring buffer
-     * @return returning {@code true} upon success and {@code false} if this queue is full.
-     */
-    boolean offer(@NotNull BytesStore bytes0) throws InterruptedException;
-
-    /**
-     * Retrieves and removes the head of this queue, or returns {@code null} if this queue is
-     * empty.
-     *
-     * @return false if this queue is empty, or a populated buffer if the element was retried
-     * @throws IllegalStateException is the {@code using} buffer is not large enough
-     */
-    boolean read(@NotNull Bytes using) throws IllegalStateException;
-
-    long readRemaining();
 
     @NotNull
     static BytesRingBuffer newInstance(@NotNull NativeBytesStore<Void> bytesStore) {
@@ -62,7 +54,6 @@ public interface BytesRingBuffer extends BytesRingBufferStats, BytesConsumer {
                 "software.chronicle.enterprise.queue.EnterpriseRingBuffer");
     }
 
-
     static long sizeFor(long capacity) {
         try {
             final Method sizeFor = clazz().getMethod("sizeFor", long.class);
@@ -75,6 +66,31 @@ public interface BytesRingBuffer extends BytesRingBufferStats, BytesConsumer {
         }
 
     }
+
+    /**
+     * clears the ring buffer but moving the read position to the write position
+     */
+    void clear();
+
+    /**
+     * Inserts the specified element at the tail of this queue if it is possible to do so
+     * immediately without exceeding the queue's capacity,
+     *
+     * @param bytes0 the {@code bytes0} that you wish to add to the ring buffer
+     * @return returning {@code true} upon success and {@code false} if this queue is full.
+     */
+    boolean offer(@NotNull BytesStore bytes0) throws InterruptedException;
+
+    /**
+     * Retrieves and removes the head of this queue, or returns {@code null} if this queue is
+     * empty.
+     *
+     * @return false if this queue is empty, or a populated buffer if the element was retried
+     * @throws IllegalStateException is the {@code using} buffer is not large enough
+     */
+    boolean read(@NotNull Bytes using) throws IllegalStateException;
+
+    long readRemaining();
 
 
 }
