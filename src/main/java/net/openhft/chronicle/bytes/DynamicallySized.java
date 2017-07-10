5e41b88635ca2e25c858f384789bfb4c6e176d39
epickrram <mark@epickrram.com>
2017-07-10 09:55:26 +0100

    add marker interface for runtime-sized Byteables

---
+++ src/main/java/net/openhft/chronicle/bytes/DynamicallySized.java
@@ -0,0 +1,8 @@
+package net.openhft.chronicle.bytes;
+
+/**
+ * Marker interface for implementors of {@link Byteable} that indicates
+ * that size requirements can only be calculated on a user-supplied instance.
+ */
+public interface DynamicallySized {
+}
