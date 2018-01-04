7bac61023d973d30aba73b4f7de27e5f930395c7
Dmitry Pisklov <dpisklov@gmail.com>
2018-01-04 18:12:45 +0000

    Unused import

--- src/test/java/net/openhft/chronicle/bytes/BytesInternalTest.java
+++ src/test/java/net/openhft/chronicle/bytes/BytesInternalTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 higherfrequencytrading.com
+ * Copyright 2016-2018 higherfrequencytrading.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
