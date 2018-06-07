cbb5de9d77bdae82789dad141266a8b28a8214b7
peter lawrey <peter.lawrey@chronicle.software>
2018-06-07 18:59:00 +0100

    Added a light weight polling mechanism OpenHFT/Chronicle-Queue#490

--- src/main/java/net/openhft/chronicle/bytes/MethodReader.java
+++ src/main/java/net/openhft/chronicle/bytes/MethodReader.java
@@ -7,9 +7,19 @@ public interface MethodReader extends Closeable {
 
     MethodReaderInterceptor methodReaderInterceptor();
 
+    /**
+     * Moves the queue to read a message if there is one, but is more expensive
+     *
+     * @return true if there was a message, false if no more messages.
+     */
     boolean readOne();
 
-    default boolean quickReadOne() {
+    /**
+     * Does a quick read which is simpler but might not read the next message. readOne() has to be called periodically.
+     *
+     * @return true if there was a message, false if there is probably not a message.
+     */
+    default boolean lazyReadOne() {
         return readOne();
     }
 
