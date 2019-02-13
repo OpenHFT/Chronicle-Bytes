8dd478405b7d1c8cc8c46fa1d7ad01de343705dd
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-02-13 17:20:10 +0000

    Add a test for BytesUtil.findFile()

--- src/test/java/net/openhft/chronicle/bytes/BytesUtilTest.java
+++ src/test/java/net/openhft/chronicle/bytes/BytesUtilTest.java
@@ -2,6 +2,8 @@ package net.openhft.chronicle.bytes;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
@@ -16,5 +18,12 @@ public class BytesUtilTest {
         Bytes bytes = BytesUtil.readFile("/net/openhft/chronicle/core/onoes/Google.properties");
         long n = bytes.indexOf(Bytes.from("Apache License"));
         assertTrue(n > 0);
+    }
+
+    @Test
+    public void findFile() throws FileNotFoundException {
+        String file = BytesUtil.findFile("file-to-find.txt");
+        assertTrue(new File(file).exists());
+        assertTrue(new File(file).canWrite());
     }
 }
