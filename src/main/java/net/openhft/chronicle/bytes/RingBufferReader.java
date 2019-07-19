36805891efb6ea48a3abe931ccf22a15ef2d660d
Jerry Shea <jerry.shea@chronicle.software>
2019-07-19 11:26:59 +1000

    make RingBufferReader Closeable

--- src/main/java/net/openhft/chronicle/bytes/RingBufferReader.java
+++ src/main/java/net/openhft/chronicle/bytes/RingBufferReader.java
@@ -1,9 +1,8 @@
 package net.openhft.chronicle.bytes;
 
-/**
- * Created by Jerry Shea on 23/04/18.
- */
-public interface RingBufferReader extends RingBufferReaderStats {
+import java.io.Closeable;
+
+public interface RingBufferReader extends RingBufferReaderStats, Closeable {
 
     boolean isEmpty();
 
