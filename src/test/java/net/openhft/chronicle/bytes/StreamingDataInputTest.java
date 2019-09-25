d83a5bb113d81361d4a6099e1e906ba97f2a9e54
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-09-25 12:47:45 +0100

    Fixed StreamingDataInput.read(byte[], offset, length) was ignoring the offset, closes https://github.com/OpenHFT/Chronicle-Map/issues/183

---
+++ src/test/java/net/openhft/chronicle/bytes/StreamingDataInputTest.java
@@ -0,0 +1,17 @@
+package net.openhft.chronicle.bytes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StreamingDataInputTest {
+    @Test
+    public void read() {
+        Bytes b = Bytes.allocateDirect(128);
+        b.append("0123456789");
+        byte[] byteArr = "ABCDEFGHIJKLMNOP".getBytes();
+        b.read(byteArr, 2, 6);
+        assertEquals("AB012345IJKLMNOP", new String(byteArr, 0));
+        assertEquals('6', b.readByte());
+    }
+}
