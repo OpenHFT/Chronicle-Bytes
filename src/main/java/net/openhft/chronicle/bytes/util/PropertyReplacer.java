9e5b0185a8ca4db35c3014a5dd38314d315004e6
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-06-16 12:07:58 +0100

    Add `throwExceptionIfClosed();` to most public methods for classes implementing Closeable, https://github.com/OpenHFT/Chronicle-Core/issues/116

--- src/main/java/net/openhft/chronicle/bytes/util/PropertyReplacer.java
+++ src/main/java/net/openhft/chronicle/bytes/util/PropertyReplacer.java
@@ -21,6 +21,7 @@ import net.openhft.chronicle.bytes.BytesUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -76,10 +77,13 @@ public enum PropertyReplacer {
     @NotNull
     public static String fileAsString(String fileName) throws IOException {
         try {
-            return convertStreamToString(PropertyReplacer.class.getResourceAsStream(fileName));
-        } catch (Exception e) {
-            return BytesUtil.readFile(fileName).toString();
+            Class<PropertyReplacer> propertyReplacerClass = PropertyReplacer.class;
+            InputStream is = propertyReplacerClass.getResourceAsStream(fileName);
+            if (is != null)
+                return convertStreamToString(is);
+        } catch (Exception ignored) {
         }
+        return BytesUtil.readFile(fileName).toString();
     }
 
     @NotNull
