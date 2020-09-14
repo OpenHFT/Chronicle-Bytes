c79349c9d0b9f7618008e850fa3335e63d7b137a
Per Minborg <minborg@speedment.com>
2020-09-14 20:48:15 +0200

    Tidy and reduce clutter

--- src/test/java/net/openhft/chronicle/bytes/BytesMethodWriterBuilderTest.java
+++ src/test/java/net/openhft/chronicle/bytes/BytesMethodWriterBuilderTest.java
@@ -21,114 +21,118 @@ public class BytesMethodWriterBuilderTest extends BytesTestCommon {
     @Test
     public void testPrimitives() {
         assumeFalse(NativeBytes.areNewGuarded());
-        Bytes<?> bytes = new HexDumpBytes();
-        IBytesMethod m = bytes.bytesMethodWriter(IBytesMethod.class);
+        final Bytes<?> bytes = new HexDumpBytes();
+        try {
+            final IBytesMethod m = bytes.bytesMethodWriter(IBytesMethod.class);
 
-        @NotNull MyByteable mb1 = new MyByteable(false, (byte) 1, (short) 2, '3', 4, 5.5f, 6, 7.7);
-        @NotNull MyByteable mb2 = new MyByteable(true, (byte) 11, (short) 22, 'T', 44, 5.555f, 66, 77.77);
+            final MyByteable mb1 = new MyByteable(false, (byte) 1, (short) 2, '3', 4, 5.5f, 6, 7.7);
+            final MyByteable mb2 = new MyByteable(true, (byte) 11, (short) 22, 'T', 44, 5.555f, 66, 77.77);
 
-        m.myByteable(mb1);
-        m.myByteable(mb2);
+            m.myByteable(mb1);
+            m.myByteable(mb2);
 
-        ZonedDateTime zdt1 = ZonedDateTime.parse("2017-11-06T12:35:56.775Z[Europe/London]");
-        ZonedDateTime zdt2 = ZonedDateTime.parse("2016-10-05T01:34:56.775Z[Europe/London]");
-        UUID uuid1 = new UUID(0x123456789L, 0xABCDEF);
-        UUID uuid2 = new UUID(0x1111111111111111L, 0x2222222222222222L);
-        @NotNull MyScalars ms1 = new MyScalars("Hello", BigInteger.ONE, BigDecimal.TEN, zdt1.toLocalDate(), zdt1.toLocalTime(), zdt1.toLocalDateTime(), zdt1, uuid1);
-        @NotNull MyScalars ms2 = new MyScalars("World", BigInteger.ZERO, BigDecimal.ZERO, zdt2.toLocalDate(), zdt2.toLocalTime(), zdt2.toLocalDateTime(), zdt2, uuid2);
-        @NotNull MyNested mn2 = new MyNested(mb2, ms2);
+            final ZonedDateTime zdt1 = ZonedDateTime.parse("2017-11-06T12:35:56.775Z[Europe/London]");
+            final ZonedDateTime zdt2 = ZonedDateTime.parse("2016-10-05T01:34:56.775Z[Europe/London]");
+            final UUID uuid1 = new UUID(0x123456789L, 0xABCDEF);
+            final UUID uuid2 = new UUID(0x1111111111111111L, 0x2222222222222222L);
+            final MyScalars ms1 = new MyScalars("Hello", BigInteger.ONE, BigDecimal.TEN, zdt1.toLocalDate(), zdt1.toLocalTime(), zdt1.toLocalDateTime(), zdt1, uuid1);
+            final MyScalars ms2 = new MyScalars("World", BigInteger.ZERO, BigDecimal.ZERO, zdt2.toLocalDate(), zdt2.toLocalTime(), zdt2.toLocalDateTime(), zdt2, uuid2);
+            final MyNested mn2 = new MyNested(mb2, ms2);
 
-        m.myScalars(ms1);
+            m.myScalars(ms1);
 
-        m.myNested(mn2);
+            m.myNested(mn2);
 
-        assertEquals("" +
-                "81 01                                           # myByteable\n" +
-                "   4e                                              # flag\n" +
-                "   01                                              # b\n" +
-                "   02 00                                           # s\n" +
-                "   33                                              # c\n" +
-                "   04 00 00 00                                     # i\n" +
-                "   00 00 b0 40                                     # f\n" +
-                "   06 00 00 00 00 00 00 00                         # l\n" +
-                "   cd cc cc cc cc cc 1e 40                         # d\n" +
-                "81 01                                           # myByteable\n" +
-                "   59                                              # flag\n" +
-                "   0b                                              # b\n" +
-                "   16 00                                           # s\n" +
-                "   54                                              # c\n" +
-                "   2c 00 00 00                                     # i\n" +
-                "   8f c2 b1 40                                     # f\n" +
-                "   42 00 00 00 00 00 00 00                         # l\n" +
-                "   e1 7a 14 ae 47 71 53 40                         # d\n" +
-                "82 01                                           # myScalars\n" +
-                "   05 48 65 6c 6c 6f                               # s\n" +
-                "   01 31                                           # bi\n" +
-                "   02 31 30                                        # bd\n" +
-                "   0a 32 30 31 37 2d 31 31 2d 30 36                # date\n" +
-                "   0c 31 32 3a 33 35 3a 35 36 2e 37 37 35          # time\n" +
-                "   17 32 30 31 37 2d 31 31 2d 30 36 54 31 32 3a 33 # dateTime\n" +
-                "   35 3a 35 36 2e 37 37 35 27 32 30 31 37 2d 31 31 # zonedDateTime\n" +
-                "   2d 30 36 54 31 32 3a 33 35 3a 35 36 2e 37 37 35\n" +
-                "   5a 5b 45 75 72 6f 70 65 2f 4c 6f 6e 64 6f 6e 5d # uuid\n" +
-                "   24 30 30 30 30 30 30 30 31 2d 32 33 34 35 2d 36\n" +
-                "   37 38 39 2d 30 30 30 30 2d 30 30 30 30 30 30 61\n" +
-                "   62 63 64 65 66\n" +
-                "83 01                                           # myNested\n" +
-                "                                                # byteable\n" +
-                "      59                                              # flag\n" +
-                "      0b                                              # b\n" +
-                "      16 00                                           # s\n" +
-                "      54                                              # c\n" +
-                "      2c 00 00 00                                     # i\n" +
-                "      8f c2 b1 40                                     # f\n" +
-                "      42 00 00 00 00 00 00 00                         # l\n" +
-                "      e1 7a 14 ae 47 71 53 40                         # d\n" +
-                "                                                # scalars\n" +
-                "      05 57 6f 72 6c 64                               # s\n" +
-                "      01 30                                           # bi\n" +
-                "      01 30                                           # bd\n" +
-                "      0a 32 30 31 36 2d 31 30 2d 30 35                # date\n" +
-                (Jvm.isJava9Plus() ?
-                        "      0c 30 32 3a 33 34 3a 35 36 2e 37 37 35          # time\n" :
-                        "      0c 30 31 3a 33 34 3a 35 36 2e 37 37 35          # time\n") +
-                (Jvm.isJava9Plus() ?
-                        "      17 32 30 31 36 2d 31 30 2d 30 35 54 30 32 3a 33 # dateTime\n" :
-                        "      17 32 30 31 36 2d 31 30 2d 30 35 54 30 31 3a 33 # dateTime\n") +
-                "      34 3a 35 36 2e 37 37 35 2c 32 30 31 36 2d 31 30 # zonedDateTime\n" +
-                (Jvm.isJava9Plus() ?
-                        "      2d 30 35 54 30 32 3a 33 34 3a 35 36 2e 37 37 35\n" :
-                        "      2d 30 35 54 30 31 3a 33 34 3a 35 36 2e 37 37 35\n") +
-                "      2b 30 31 3a 30 30 5b 45 75 72 6f 70 65 2f 4c 6f\n" +
-                "      6e 64 6f 6e 5d 24 31 31 31 31 31 31 31 31 2d 31 # uuid\n" +
-                "      31 31 31 2d 31 31 31 31 2d 32 32 32 32 2d 32 32\n" +
-                "      32 32 32 32 32 32 32 32 32 32\n", bytes.toHexString());
+            assertEquals("" +
+                    "81 01                                           # myByteable\n" +
+                    "   4e                                              # flag\n" +
+                    "   01                                              # b\n" +
+                    "   02 00                                           # s\n" +
+                    "   33                                              # c\n" +
+                    "   04 00 00 00                                     # i\n" +
+                    "   00 00 b0 40                                     # f\n" +
+                    "   06 00 00 00 00 00 00 00                         # l\n" +
+                    "   cd cc cc cc cc cc 1e 40                         # d\n" +
+                    "81 01                                           # myByteable\n" +
+                    "   59                                              # flag\n" +
+                    "   0b                                              # b\n" +
+                    "   16 00                                           # s\n" +
+                    "   54                                              # c\n" +
+                    "   2c 00 00 00                                     # i\n" +
+                    "   8f c2 b1 40                                     # f\n" +
+                    "   42 00 00 00 00 00 00 00                         # l\n" +
+                    "   e1 7a 14 ae 47 71 53 40                         # d\n" +
+                    "82 01                                           # myScalars\n" +
+                    "   05 48 65 6c 6c 6f                               # s\n" +
+                    "   01 31                                           # bi\n" +
+                    "   02 31 30                                        # bd\n" +
+                    "   0a 32 30 31 37 2d 31 31 2d 30 36                # date\n" +
+                    "   0c 31 32 3a 33 35 3a 35 36 2e 37 37 35          # time\n" +
+                    "   17 32 30 31 37 2d 31 31 2d 30 36 54 31 32 3a 33 # dateTime\n" +
+                    "   35 3a 35 36 2e 37 37 35 27 32 30 31 37 2d 31 31 # zonedDateTime\n" +
+                    "   2d 30 36 54 31 32 3a 33 35 3a 35 36 2e 37 37 35\n" +
+                    "   5a 5b 45 75 72 6f 70 65 2f 4c 6f 6e 64 6f 6e 5d # uuid\n" +
+                    "   24 30 30 30 30 30 30 30 31 2d 32 33 34 35 2d 36\n" +
+                    "   37 38 39 2d 30 30 30 30 2d 30 30 30 30 30 30 61\n" +
+                    "   62 63 64 65 66\n" +
+                    "83 01                                           # myNested\n" +
+                    "                                                # byteable\n" +
+                    "      59                                              # flag\n" +
+                    "      0b                                              # b\n" +
+                    "      16 00                                           # s\n" +
+                    "      54                                              # c\n" +
+                    "      2c 00 00 00                                     # i\n" +
+                    "      8f c2 b1 40                                     # f\n" +
+                    "      42 00 00 00 00 00 00 00                         # l\n" +
+                    "      e1 7a 14 ae 47 71 53 40                         # d\n" +
+                    "                                                # scalars\n" +
+                    "      05 57 6f 72 6c 64                               # s\n" +
+                    "      01 30                                           # bi\n" +
+                    "      01 30                                           # bd\n" +
+                    "      0a 32 30 31 36 2d 31 30 2d 30 35                # date\n" +
+                    (Jvm.isJava9Plus() ?
+                            "      0c 30 32 3a 33 34 3a 35 36 2e 37 37 35          # time\n" :
+                            "      0c 30 31 3a 33 34 3a 35 36 2e 37 37 35          # time\n") +
+                    (Jvm.isJava9Plus() ?
+                            "      17 32 30 31 36 2d 31 30 2d 30 35 54 30 32 3a 33 # dateTime\n" :
+                            "      17 32 30 31 36 2d 31 30 2d 30 35 54 30 31 3a 33 # dateTime\n") +
+                    "      34 3a 35 36 2e 37 37 35 2c 32 30 31 36 2d 31 30 # zonedDateTime\n" +
+                    (Jvm.isJava9Plus() ?
+                            "      2d 30 35 54 30 32 3a 33 34 3a 35 36 2e 37 37 35\n" :
+                            "      2d 30 35 54 30 31 3a 33 34 3a 35 36 2e 37 37 35\n") +
+                    "      2b 30 31 3a 30 30 5b 45 75 72 6f 70 65 2f 4c 6f\n" +
+                    "      6e 64 6f 6e 5d 24 31 31 31 31 31 31 31 31 2d 31 # uuid\n" +
+                    "      31 31 31 2d 31 31 31 31 2d 32 32 32 32 2d 32 32\n" +
+                    "      32 32 32 32 32 32 32 32 32 32\n", bytes.toHexString());
 
-        StringWriter out = new StringWriter();
-        MethodReader reader = bytes.bytesMethodReader(Mocker.logging(IBytesMethod.class, "* ", out));
+            final StringWriter out = new StringWriter();
+            final MethodReader reader = bytes.bytesMethodReader(Mocker.logging(IBytesMethod.class, "* ", out));
 
-        for (int i = 0; i < 4; i++) {
-            assertTrue(reader.readOne());
+            for (int i = 0; i < 4; i++) {
+                assertTrue(reader.readOne());
+            }
+            assertFalse(reader.readOne());
+
+            final String expected =
+                    Jvm.isJava9Plus() ?
+                            "* myByteable[MyByteable{flag=false, b=1, s=2, c=3, i=4, f=5.5, l=6, d=7.7}]\n" +
+                                    "* myByteable[MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}]\n" +
+                                    "* myScalars[MyScalars{s='Hello', bi=1, bd=10, date=2017-11-06, time=12:35:56.775, dateTime=2017-11-06T12:35:56.775, zonedDateTime=2017-11-06T12:35:56.775Z[Europe/London], uuid=00000001-2345-6789-0000-000000abcdef}]\n" +
+                                    "* myNested[MyNested{byteable=MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}, scalars=MyScalars{s='World', bi=0, bd=0, date=2016-10-05, time=02:34:56.775, dateTime=2016-10-05T02:34:56.775, zonedDateTime=2016-10-05T02:34:56.775+01:00[Europe/London], uuid=11111111-1111-1111-2222-222222222222}}]\n" :
+                            "* myByteable[MyByteable{flag=false, b=1, s=2, c=3, i=4, f=5.5, l=6, d=7.7}]\n" +
+                                    "* myByteable[MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}]\n" +
+                                    "* myScalars[MyScalars{s='Hello', bi=1, bd=10, date=2017-11-06, time=12:35:56.775, dateTime=2017-11-06T12:35:56.775, zonedDateTime=2017-11-06T12:35:56.775Z[Europe/London], uuid=00000001-2345-6789-0000-000000abcdef}]\n" +
+                                    "* myNested[MyNested{byteable=MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}, scalars=MyScalars{s='World', bi=0, bd=0, date=2016-10-05, time=01:34:56.775, dateTime=2016-10-05T01:34:56.775, zonedDateTime=2016-10-05T01:34:56.775+01:00[Europe/London], uuid=11111111-1111-1111-2222-222222222222}}]\n";
+
+/*        System.out.println(expected);
+        System.out.println(out.toString().replaceAll("\n", ""));*/
+
+            assertEquals(expected,
+                    out.toString().replaceAll("\r", ""));
+        } finally {
+            bytes.releaseLast();
         }
-        assertFalse(reader.readOne());
 
-        final String expected =
-                Jvm.isJava9Plus() ?
-                        "* myByteable[MyByteable{flag=false, b=1, s=2, c=3, i=4, f=5.5, l=6, d=7.7}]\n" +
-                                "* myByteable[MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}]\n" +
-                                "* myScalars[MyScalars{s='Hello', bi=1, bd=10, date=2017-11-06, time=12:35:56.775, dateTime=2017-11-06T12:35:56.775, zonedDateTime=2017-11-06T12:35:56.775Z[Europe/London], uuid=00000001-2345-6789-0000-000000abcdef}]\n" +
-                                "* myNested[MyNested{byteable=MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}, scalars=MyScalars{s='World', bi=0, bd=0, date=2016-10-05, time=02:34:56.775, dateTime=2016-10-05T02:34:56.775, zonedDateTime=2016-10-05T02:34:56.775+01:00[Europe/London], uuid=11111111-1111-1111-2222-222222222222}}]\n" :
-                        "* myByteable[MyByteable{flag=false, b=1, s=2, c=3, i=4, f=5.5, l=6, d=7.7}]\n" +
-                                "* myByteable[MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}]\n" +
-                                "* myScalars[MyScalars{s='Hello', bi=1, bd=10, date=2017-11-06, time=12:35:56.775, dateTime=2017-11-06T12:35:56.775, zonedDateTime=2017-11-06T12:35:56.775Z[Europe/London], uuid=00000001-2345-6789-0000-000000abcdef}]\n" +
-                                "* myNested[MyNested{byteable=MyByteable{flag=true, b=11, s=22, c=T, i=44, f=5.555, l=66, d=77.77}, scalars=MyScalars{s='World', bi=0, bd=0, date=2016-10-05, time=01:34:56.775, dateTime=2016-10-05T01:34:56.775, zonedDateTime=2016-10-05T01:34:56.775+01:00[Europe/London], uuid=11111111-1111-1111-2222-222222222222}}]\n";
-
-        System.out.println(expected);
-        System.out.println(out.toString().replaceAll("\n", ""));
-
-        assertEquals(expected,
-                out.toString().replaceAll("\r", ""));
-        bytes.releaseLast();
     }
 
 }
--- src/test/java/net/openhft/chronicle/bytes/MappedFileTest.java
+++ src/test/java/net/openhft/chronicle/bytes/MappedFileTest.java
@@ -23,8 +23,6 @@ import net.openhft.chronicle.core.io.AbstractReferenceCounted;
 import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.core.io.ReferenceOwner;
 import net.openhft.chronicle.core.threads.ThreadDump;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
@@ -36,8 +34,9 @@ import java.nio.BufferUnderflowException;
 import static org.junit.Assert.*;
 
 public class MappedFileTest extends BytesTestCommon {
+
     @Rule
-    public TemporaryFolder tmpDir = new TemporaryFolder();
+    public final TemporaryFolder tmpDir = new TemporaryFolder();
     private ThreadDump threadDump;
 
     @After
@@ -65,11 +64,11 @@ public class MappedFileTest extends BytesTestCommon {
         final File file = tmpDir.newFile();
         // this is what it will end up as
         final long chunkSize = OS.mapAlign(64);
-        ReferenceOwner test = ReferenceOwner.temporary("test");
+        final ReferenceOwner test = ReferenceOwner.temporary("test");
         try (final MappedFile mappedFile = MappedFile.mappedFile(file, 64)) {
             final MappedBytesStore first = mappedFile.acquireByteStore(test, 1);
 
-            int expected = MappedFile.RETAIN ? 2 : 1;
+            final int expected = MappedFile.RETAIN ? 2 : 1;
             assertEquals(expected, first.refCount());
 
             final MappedBytesStore second = mappedFile.acquireByteStore(test, 1 + chunkSize);
@@ -89,55 +88,57 @@ public class MappedFileTest extends BytesTestCommon {
         }
     }
 
-    @SuppressWarnings("rawtypes")
     @Test
     public void testReferenceCounts() throws IOException {
-        @NotNull File tmp = IOTools.createTempFile("testReferenceCounts");
-        int chunkSize = OS.isWindows() ? 64 << 10 : 4 << 10;
+        final File tmp = IOTools.createTempFile("testReferenceCounts");
+        final int chunkSize = OS.isWindows() ? 64 << 10 : 4 << 10;
         try (MappedFile mf = MappedFile.mappedFile(tmp, chunkSize, 0)) {
             assertEquals("refCount: 1", mf.referenceCounts());
 
-            ReferenceOwner test = ReferenceOwner.temporary("test");
-            @Nullable MappedBytesStore bs = mf.acquireByteStore(test, chunkSize + (1 << 10));
-            assertEquals(chunkSize, bs.start());
-            assertEquals(chunkSize * 2, bs.capacity());
-            Bytes bytes = bs.bytesForRead();
-
-            assertNotNull(bytes.toString()); // show it doesn't blow up.
-            assertNotNull(bs.toString()); // show it doesn't blow up.
-            assertEquals(chunkSize, bytes.start());
-            assertEquals(0L, bs.readLong(chunkSize + (1 << 10)));
-            assertEquals(0L, bytes.readLong(chunkSize + (1 << 10)));
-            Assert.assertFalse(bs.inside(chunkSize - (1 << 10)));
-            Assert.assertFalse(bs.inside(chunkSize - 1));
-            Assert.assertTrue(bs.inside(chunkSize));
-            Assert.assertTrue(bs.inside(chunkSize * 2 - 1));
-            Assert.assertFalse(bs.inside(chunkSize * 2));
+            final ReferenceOwner test = ReferenceOwner.temporary("test");
+            final MappedBytesStore bs = mf.acquireByteStore(test, chunkSize + (1 << 10));
             try {
-                bytes.readLong(chunkSize - (1 << 10));
-                Assert.fail();
-            } catch (BufferUnderflowException e) {
-                // expected
-            }
-            try {
-                bytes.readLong(chunkSize * 2 + (1 << 10));
-                Assert.fail();
-            } catch (BufferUnderflowException e) {
-                // expected
-            }
-            assertEquals(1, mf.refCount());
-            int expected = MappedFile.RETAIN ? 2 : 1;
-            assertEquals(expected + 1, bs.refCount());
-            assertEquals("refCount: 1, 0, " + (expected + 1), mf.referenceCounts());
+                assertEquals(chunkSize, bs.start());
+                assertEquals(chunkSize * 2, bs.capacity());
+                final Bytes<?> bytes = bs.bytesForRead();
 
-            @Nullable BytesStore bs2 = mf.acquireByteStore(test, chunkSize + (1 << 10), bs);
-            assertSame(bs, bs2);
-            assertEquals(expected + 1, bs2.refCount());
-            assertEquals("refCount: 1, 0, " + (expected + 1), mf.referenceCounts());
-            bytes.releaseLast();
-            assertEquals(expected, bs2.refCount());
-            assertEquals("refCount: 1, 0, " + expected, mf.referenceCounts());
-            bs.release(test);
+                assertNotNull(bytes.toString()); // show it doesn't blow up.
+                assertNotNull(bs.toString()); // show it doesn't blow up.
+                assertEquals(chunkSize, bytes.start());
+                assertEquals(0L, bs.readLong(chunkSize + (1 << 10)));
+                assertEquals(0L, bytes.readLong(chunkSize + (1 << 10)));
+                Assert.assertFalse(bs.inside(chunkSize - (1 << 10)));
+                Assert.assertFalse(bs.inside(chunkSize - 1));
+                Assert.assertTrue(bs.inside(chunkSize));
+                Assert.assertTrue(bs.inside(chunkSize * 2 - 1));
+                Assert.assertFalse(bs.inside(chunkSize * 2));
+                try {
+                    bytes.readLong(chunkSize - (1 << 10));
+                    Assert.fail();
+                } catch (BufferUnderflowException e) {
+                    // expected
+                }
+                try {
+                    bytes.readLong(chunkSize * 2 + (1 << 10));
+                    Assert.fail();
+                } catch (BufferUnderflowException e) {
+                    // expected
+                }
+                assertEquals(1, mf.refCount());
+                final int expected = MappedFile.RETAIN ? 2 : 1;
+                assertEquals(expected + 1, bs.refCount());
+                assertEquals("refCount: 1, 0, " + (expected + 1), mf.referenceCounts());
+
+                final BytesStore<?, ?> bs2 = mf.acquireByteStore(test, chunkSize + (1 << 10), bs);
+                assertSame(bs, bs2);
+                assertEquals(expected + 1, bs2.refCount());
+                assertEquals("refCount: 1, 0, " + (expected + 1), mf.referenceCounts());
+                bytes.releaseLast();
+                assertEquals(expected, bs2.refCount());
+                assertEquals("refCount: 1, 0, " + expected, mf.referenceCounts());
+            } finally {
+                bs.release(test);
+            }
         }
     }
 
@@ -146,13 +147,13 @@ public class MappedFileTest extends BytesTestCommon {
         if (Runtime.getRuntime().totalMemory() < Integer.MAX_VALUE || OS.isWindows())
             return;
 
-        @NotNull File file = File.createTempFile("largeReadOnlyFile", "deleteme");
+        final File file = File.createTempFile("largeReadOnlyFile", "deleteme");
         file.deleteOnExit();
-        try (@NotNull MappedBytes bytes = MappedBytes.mappedBytes(file, 1 << 30, OS.pageSize())) {
+        try (MappedBytes bytes = MappedBytes.mappedBytes(file, 1 << 30, OS.pageSize())) {
             bytes.writeLong(3L << 30, 0x12345678); // make the file 3 GB.
         }
 
-        try (@NotNull MappedBytes bytes = MappedBytes.readOnly(file)) {
+        try (MappedBytes bytes = MappedBytes.readOnly(file)) {
             Assert.assertEquals(0x12345678L, bytes.readLong(3L << 30));
         }
     }
@@ -160,7 +161,7 @@ public class MappedFileTest extends BytesTestCommon {
     @Test
     public void interrupted() throws FileNotFoundException {
         Thread.currentThread().interrupt();
-        String filename = IOTools.createTempFile("interrupted").getAbsolutePath();
+        final String filename = IOTools.createTempFile("interrupted").getAbsolutePath();
         try (MappedFile mf = MappedFile.mappedFile(filename, 64 << 10, 0)) {
             mf.actualSize();
             assertTrue(Thread.currentThread().isInterrupted());
@@ -169,13 +170,15 @@ public class MappedFileTest extends BytesTestCommon {
 
     @Test
     public void testCreateMappedFile() throws IOException {
-        File file = IOTools.createTempFile("mappedFile");
+        final File file = IOTools.createTempFile("mappedFile");
 
-        MappedFile mappedFile = MappedFile.mappedFile(file, 1024, 256, 256, false);
-        MappedFile mappedFile2 = MappedFile.mappedFile(file, 1024, 256, 256, false);
-
-        mappedFile.releaseLast();
-        mappedFile2.releaseLast();
+        final MappedFile mappedFile = MappedFile.mappedFile(file, 1024, 256, 256, false);
+        try {
+            final MappedFile mappedFile2 = MappedFile.mappedFile(file, 1024, 256, 256, false);
+            mappedFile2.releaseLast();
+        } finally {
+            mappedFile.releaseLast();
+        }
     }
 
     @After
