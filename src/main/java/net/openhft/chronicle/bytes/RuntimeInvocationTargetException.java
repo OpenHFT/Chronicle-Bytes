c9fa0b7ea3e702714ccb43ecde97b78556664e83
Jerry Shea <jerry.shea@chronicle.software>
2020-12-03 11:32:35 +1100

    RuntimeInvocationTargetException, doco https://github.com/OpenHFT/Chronicle-Wire/issues/243

--- src/main/java/net/openhft/chronicle/bytes/RuntimeInvocationTargetException.java
+++ src/main/java/net/openhft/chronicle/bytes/RuntimeInvocationTargetException.java
@@ -1,10 +1,14 @@
 package net.openhft.chronicle.bytes;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Thrown if the receiver of the {@link MethodReader} throws an exception on invocation
  */
 public class RuntimeInvocationTargetException extends RuntimeException {
-    public RuntimeInvocationTargetException(Throwable cause) {
-        super(cause);
+    public RuntimeInvocationTargetException(@NotNull Throwable cause) {
+        super(cause instanceof InvocationTargetException ? cause.getCause() : cause);
     }
 }
