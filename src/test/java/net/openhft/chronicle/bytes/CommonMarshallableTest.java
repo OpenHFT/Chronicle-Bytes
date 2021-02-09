8e7b8c59c921b2fca1daf697f8fc9e79f35f5c4c
Peter Lawrey <peter.lawrey@chronicle.software>
2021-02-09 13:23:18 +0000

    Wires.copyTo now works for mismatched BytesInBinaryMarshallable objects, closes https://github.com/OpenHFT/Chronicle-Wire/issues/260

--- src/test/java/net/openhft/chronicle/bytes/CommonMarshallableTest.java
+++ src/test/java/net/openhft/chronicle/bytes/CommonMarshallableTest.java
@@ -2,13 +2,13 @@ package net.openhft.chronicle.bytes;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CommonMarshallableTest extends BytesTestCommon {
 
     @Test
     public void usesSelfDescribingMessage() {
-        assertFalse(new CommonMarshallable() {
+        assertTrue(new CommonMarshallable() {
         }.usesSelfDescribingMessage());
     }
 }
