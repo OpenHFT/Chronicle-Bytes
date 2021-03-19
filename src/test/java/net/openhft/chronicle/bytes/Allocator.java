18e8302fc91fd6b688a90d41874257940dc72bab
Peter Lawrey <peter.lawrey@chronicle.software>
2021-03-19 11:43:55 +0000

    Added EmbeddedBytes for Trivially Copyable data transfer objects, https://github.com/OpenHFT/Chronicle-Bytes/issues/173

--- src/test/java/net/openhft/chronicle/bytes/Allocator.java
+++ src/test/java/net/openhft/chronicle/bytes/Allocator.java
@@ -124,6 +124,7 @@ public enum Allocator {
     }
 
     static class Padding extends Parent {
+        @FieldGroup("p")
         // 128 bytes
         transient long p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15;
     }
--- src/test/java/net/openhft/chronicle/bytes/internal/BytesFieldInfoTest.java
+++ src/test/java/net/openhft/chronicle/bytes/internal/BytesFieldInfoTest.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.bytes.internal;
 
+import net.openhft.chronicle.bytes.FieldGroup;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,12 +10,17 @@ public class BytesFieldInfoTest {
     @Test
     public void lookup() {
         final BytesFieldInfo lookup = BytesFieldInfo.lookup(Groups1.class);
-        assertEquals("type: BytesFieldInfo, groups: { hi: 12 to 16, pad: 16 to 48, add: 48 to 64 }", lookup.dump());
+        assertEquals("type: BytesFieldInfo, groups: { pad: 16 to 48, hi: 48 to 52, add: 52 to 68 }", lookup.dump());
     }
 
     static class Groups1 {
+        @FieldGroup("header")
+        int header;
+        @FieldGroup("pad")
         long pad0, pad1, pad3, pad2;
+        @FieldGroup("hi")
         int hi99;
+        @FieldGroup("add")
         int add1, add2, add3, add4;
     }
 }
