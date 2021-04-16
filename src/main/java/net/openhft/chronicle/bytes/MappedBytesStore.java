1c73c58c08c476fd4745dae76e123417323f7239
Peter Lawrey <peter.lawrey@chronicle.software>
2021-04-16 11:08:12 +0100

    Segmentation fault using ByteStore operations, closes https://github.com/OpenHFT/Chronicle-Bytes/issues/186

--- src/main/java/net/openhft/chronicle/bytes/MappedBytesStore.java
+++ src/main/java/net/openhft/chronicle/bytes/MappedBytesStore.java
@@ -26,8 +26,9 @@ import java.nio.BufferUnderflowException;
 
 /**
  * BytesStore to wrap memory mapped data.
+ * <p>
+ * <b>WARNING: Handle with care as this assumes the caller has correct bounds checking</b>
  */
-@Deprecated(/* to be moved to internal in x.22*/)
 public class MappedBytesStore extends NativeBytesStore<Void> {
     private final MappedFile mappedFile;
     private final long start;
