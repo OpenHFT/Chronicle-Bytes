cc77d1353da56440f1dccaec93db79f34ff1ebcc
Per-Åke Minborg <minborg@speedment.com>
2021-11-01 20:12:09 +0100

    Improve testing (#256)

---
+++ src/test/java/net/openhft/chronicle/bytes/BytesFactoryUtil.java
@@ -0,0 +1,123 @@
+package net.openhft.chronicle.bytes;
+
+import net.openhft.chronicle.core.io.ReferenceCounted;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.stream.Stream;
+
+import static net.openhft.chronicle.bytes.BytesStore.wrap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+final class BytesFactoryUtil {
+
+    private BytesFactoryUtil() {
+    }
+
+    static final int SIZE = 128;
+    static final int CHUNK_SIZE = 1024;
+
+    static Stream<Arguments> provideBytesObjects() {
+        final ByteBuffer heapByteBuffer = ByteBuffer.allocate(SIZE);
+        final ByteBuffer directByteBuffer = ByteBuffer.allocateDirect(SIZE);
+
+        try {
+            final File file = create(File.createTempFile("mapped-file", "bin"), SIZE);
+            final File fileRo = create(File.createTempFile("mapped-file-ro", "bin"), SIZE);
+            final File singleFile = create(File.createTempFile("single-mapped-file", "bin"), SIZE);
+            final File singleFileRo = create(File.createTempFile("single-mapped-file-ro", "bin"), SIZE);
+            final MassiveFieldHolder holder = new MassiveFieldHolder();
+            return Stream.of(
+                    Arguments.of(Bytes.allocateDirect(SIZE), true, "Bytes.allocateDirect(SIZE)"),
+                    Arguments.of(Bytes.allocateElasticOnHeap(SIZE), true, "Bytes.allocateElasticOnHeap(SIZE)"),
+                    Arguments.of(wipe(Bytes.allocateElasticDirect()), true, "Bytes.allocateElasticDirect()"),
+                    Arguments.of(Bytes.wrapForWrite(heapByteBuffer), true, "Bytes.wrapForWrite(heapByteBuffer)"),
+                    Arguments.of(Bytes.wrapForWrite(directByteBuffer), true, "Bytes.wrapForWrite(directByteBuffer)"),
+                    Arguments.of(MappedBytes.mappedBytes(file, CHUNK_SIZE), true, "MappedBytes.mappedBytes(file, SIZE)"),
+                    Arguments.of(MappedBytes.mappedBytes(fileRo, CHUNK_SIZE, 0, true), false, "MappedBytes.mappedBytes(fileRo, SIZE, 0, true)"),
+                    Arguments.of(MappedBytes.singleMappedBytes(singleFile, CHUNK_SIZE), true, "MappedBytes.singleMappedBytes(singleFile, SIZE)"),
+                    Arguments.of(MappedBytes.singleMappedBytes(singleFileRo, CHUNK_SIZE, true), false, "MappedBytes.singleMappedBytes(singleFileRo, SIZE, true)"),
+
+                    // Todo: reactivate this one once https://github.com/OpenHFT/Chronicle-Bytes/issues/254 is fixed
+                    // Arguments.of(Bytes.forFieldGroup(holder, "b"), true, "Bytes.forFieldGroup(holder, \"b\")"),
+
+                    Arguments.of(new UncheckedBytes<>(Bytes.allocateDirect(SIZE)), true, "new UncheckedBytes<>(Bytes.allocateDirect(SIZE))"),
+                    Arguments.of(new UncheckedBytes<>(Bytes.wrapForWrite(new byte[SIZE])), true, "new UncheckedBytes<>(Bytes.wrapForWrite(new byte[SIZE]))"),
+                    Arguments.of(new HexDumpBytes(), true, "new HexDumpBytes()"),
+                    Arguments.of(Bytes.allocateDirect(SIZE).unchecked(true), true, "Bytes.allocateDirect(SIZE).unchecked(true)"),
+                    Arguments.of(Bytes.allocateElasticOnHeap(SIZE).unchecked(true), true, "Bytes.allocateElasticOnHeap(SIZE).unchecked(true)"),
+                    Arguments.of(new GuardedNativeBytes<>(wrap(ByteBuffer.allocate(SIZE)), SIZE), true, "new GuardedNativeBytes<>(wrap(ByteBuffer.allocate(SIZE))")
+            );
+        } catch (IOException ioException) {
+            throw new AssertionError("Unable to create Bytes", ioException);
+        }
+    }
+
+    static Bytes<Object> bytes(Arguments arguments) {
+        return ((Bytes<Object>) arguments.get()[0]);
+    }
+
+    static boolean isReadWrite(Arguments arguments) {
+        return Boolean.TRUE.equals(arguments.get()[1]);
+    }
+
+    static String createCommand(Arguments arguments) {
+        return arguments.get()[2].toString();
+    }
+
+    static <B extends Bytes<U>, U> B wipe(B bytes) {
+        for (int i = 0; i < SIZE; i++) {
+            bytes.writeByte((byte) 0);
+        }
+        return bytes;
+    }
+
+    static void releaseAndAssertReleased(ReferenceCounted referenceCounted) {
+        referenceCounted.releaseLast();
+        assertEquals(0, referenceCounted.refCount());
+    }
+
+    static void assertNeverWrittenTo(final Bytes<Object> bytes) {
+        assertTrue(bytes.isClear());
+        for (int i = 0; i < SIZE; i++) {
+            assertEquals(0, bytes.readByte(i), "at " + i);
+        }
+    }
+
+    static final class MassiveFieldHolder {
+
+        @FieldGroup("b")
+        long a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, aa, ab, ac, ad, ae, af;
+        //final byte[] bytes = new byte[SIZE];
+
+        static {
+            // Fields above must match
+            assert SIZE == Long.BYTES * 8 * 2;
+        }
+
+    }
+
+    static File truncate(final File file) {
+        try (FileChannel fc = FileChannel.open(file.toPath(), StandardOpenOption.WRITE)) {
+            fc.truncate(0);
+            return file;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static File create(final File file, int byteSize) {
+        try {
+            Files.write(file.toPath(), new byte[byteSize], StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+            return file;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
