796f5d47e5402067447096c7b7be7a3e2f3a8506
Per Minborg <minborg@speedment.com>
2021-11-09 16:59:15 +0100

    Improve ReferenceOwner handling

--- src/main/java/net/openhft/chronicle/bytes/internal/migration/HashCodeEqualsUtil.java
+++ src/main/java/net/openhft/chronicle/bytes/internal/migration/HashCodeEqualsUtil.java
@@ -4,6 +4,7 @@ import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesStore;
 import net.openhft.chronicle.bytes.algo.BytesStoreHash;
 import net.openhft.chronicle.bytes.internal.BytesInternal;
+import net.openhft.chronicle.core.io.ReferenceOwner;
 
 public final class HashCodeEqualsUtil {
 
@@ -13,31 +14,44 @@ public final class HashCodeEqualsUtil {
     public static boolean equals(final Bytes<?> bytes,
                                  final Object other) {
 
+        if (bytes == other) {
+            return true;
+        }
         if (!(other instanceof BytesStore)) {
             return false;
         }
         final BytesStore<?, ?> otherByteStore = (BytesStore<?, ?>) other;
-        bytes.reserve(bytes);
+
+        // Now we know that we have two different objects so we may
+        // use the same owner for them both
+        final ReferenceOwner owner = ReferenceOwner.temporary("equals");
+        bytes.reserve(owner);
         try {
-            final long remaining = bytes.readRemaining();
+            otherByteStore.reserve(owner);
             try {
-                return (otherByteStore.readRemaining() == remaining) &&
-                        BytesInternal.contentEqual(bytes, otherByteStore);
-            } catch (IllegalStateException e) {
-                return false;
+                final long remaining = bytes.readRemaining();
+                try {
+                    return (otherByteStore.readRemaining() == remaining) &&
+                            BytesInternal.contentEqual(bytes, otherByteStore);
+                } catch (IllegalStateException e) {
+                    return false;
+                }
+            } finally {
+                otherByteStore.release(owner);
             }
         } finally {
-            bytes.release(bytes);
+            bytes.release(owner);
         }
     }
 
     public static int hashCode(final Bytes<?> bytes) {
         // Reserving prevents illegal access to this Bytes object if released by another thread
-        bytes.reserve(bytes);
+        final ReferenceOwner owner = ReferenceOwner.temporary("hashCode");
+        bytes.reserve(owner);
         try {
             return BytesStoreHash.hash32(bytes);
         } finally {
-            bytes.release(bytes);
+            bytes.release(owner);
         }
     }
 
