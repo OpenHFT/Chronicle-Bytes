8c194b3a80ae0b2b85f78f8f1a196ee0af5f47b0
Per Minborg <minborg@speedment.com>
2021-11-22 14:34:44 +0100

    Rename class

--- microbenchmarks/src/main/java/net/openhft/chronicle/bytes/microbenchmarks/jmh/ElasticBenchmarkRunner.java
+++ microbenchmarks/src/main/java/net/openhft/chronicle/bytes/microbenchmarks/jmh/ElasticBenchmarkRunner.java
@@ -5,7 +5,7 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public class BenchRunner {
+public class ElasticBenchmarkRunner {
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
