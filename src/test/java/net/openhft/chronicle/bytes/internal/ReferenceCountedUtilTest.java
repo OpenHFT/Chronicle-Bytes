840faed9926769be93bdc5cf6665dce745e4ca6b
Per-Åke Minborg <minborg@speedment.com>
2021-11-25 12:12:31 +0100

    Provide more info in ReferenceCountedUtil. Fix #308 (#309)

---
+++ src/test/java/net/openhft/chronicle/bytes/internal/ReferenceCountedUtilTest.java
@@ -0,0 +1,33 @@
+package net.openhft.chronicle.bytes.internal;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.io.ClosedIllegalStateException;
+import net.openhft.chronicle.core.io.ReferenceCounted;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReferenceCountedUtilTest {
+
+    @Test
+    void throwExceptionIfReleased() {
+        test(o -> ReferenceCountedUtil.throwExceptionIfReleased((ReferenceCounted) o));
+    }
+
+    @Test
+    void testThrowExceptionIfReleased() {
+        test(ReferenceCountedUtil::throwExceptionIfReleased);
+        assertDoesNotThrow(() -> ReferenceCountedUtil.throwExceptionIfReleased("Foo"));
+        assertThrows(NullPointerException.class, () -> ReferenceCountedUtil.throwExceptionIfReleased(null));
+    }
+
+    private void test(Consumer<Object> method) {
+        final Bytes<?> bytes = Bytes.from("A");
+        assertDoesNotThrow(() -> method.accept(bytes));
+        bytes.releaseLast();
+        assertThrows(ClosedIllegalStateException.class, () -> method.accept(bytes));
+    }
+
+}
