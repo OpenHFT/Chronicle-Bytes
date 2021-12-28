72ff740b2befa52a06ad54405b7e44435f3be20b
Peter Lawrwy <peter.lawrey@chronicle.software>
2021-12-28 13:59:43 +0000

    Added a DistributedUniqueTimeProvider to make it easier to have unique identifiers across a cluster, https://github.com/OpenHFT/Chronicle-Bytes/issues/322

---
+++ src/main/java/net/openhft/chronicle/bytes/DistributedUniqueTimeProvider.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2016-2020 chronicle.software
+ *
+ * https://chronicle.software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.openhft.chronicle.bytes;
+
+import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.core.io.IORuntimeException;
+import net.openhft.chronicle.core.io.IOTools;
+import net.openhft.chronicle.core.io.SimpleCloseable;
+import net.openhft.chronicle.core.time.SystemTimeProvider;
+import net.openhft.chronicle.core.time.TimeProvider;
+
+import java.io.File;
+
+/**
+ * Timestamps while are unique across systems using a predefined hostId
+ */
+public class DistributedUniqueTimeProvider extends SimpleCloseable implements TimeProvider {
+    private static final String USE_NAME = System.getProperty("user.name", "unknown");
+    private static final String TIME_STAMP_DIR = System.getProperty("timestamp.dir", OS.TMP);
+    private static final String TIME_STAMP_PATH = System.getProperty("timestamp.path", TIME_STAMP_DIR + File.pathSeparator + ".time-stamp." + USE_NAME + ".dat");
+    private static final Integer DEFAULT_HOST_ID = Integer.getInteger("hostId", 0);
+    public static final DistributedUniqueTimeProvider INSTANCE = new DistributedUniqueTimeProvider(DEFAULT_HOST_ID, true);
+    private static final int LAST_TIME = 128;
+    private static final int HOST_IDS = 100;
+    @SuppressWarnings("rawtypes")
+    private final Bytes bytes;
+    private final MappedFile file;
+    private TimeProvider provider = SystemTimeProvider.INSTANCE;
+    private int hostId;
+
+    private DistributedUniqueTimeProvider(int hostId, boolean unmonitor) {
+        hostId(hostId);
+        try {
+            file = MappedFile.ofSingle(new File(TIME_STAMP_PATH), OS.pageSize(), false);
+            bytes = file.acquireBytesForWrite(this, 0);
+            bytes.append8bit("&TSF\nTime stamp file uses for sharing a unique id\n");
+            if (unmonitor) {
+                IOTools.unmonitor(file);
+                IOTools.unmonitor(bytes);
+            }
+
+        } catch (Exception ioe) {
+            throw new IORuntimeException(ioe);
+        }
+    }
+
+    public static DistributedUniqueTimeProvider forHostId(int hostId) {
+        return new DistributedUniqueTimeProvider(hostId, false);
+    }
+
+    public DistributedUniqueTimeProvider hostId(int hostId) {
+        if (hostId < 0)
+            throw new IllegalArgumentException("Invalid hostId: " + hostId);
+        this.hostId = hostId % HOST_IDS;
+        return this;
+    }
+
+    public DistributedUniqueTimeProvider provider(TimeProvider provider) {
+        this.provider = provider;
+        return this;
+    }
+
+    /**
+     * @return Ordinary millisecond timestamp
+     */
+    @Override
+    public long currentTimeMillis() {
+        return provider.currentTimeMillis();
+    }
+
+    /**
+     * Return a unique, monotonically increasing microsecond timestamp where the lowest two digits of the microseconds is the hostId.
+     *
+     * @return the timestamps with hostId as a long
+     * @throws IllegalStateException if the underlying resources have been released/closed
+     */
+    @Override
+    public long currentTimeMicros() throws IllegalStateException {
+        long timeus = provider.currentTimeMicros() / HOST_IDS;
+        while (true) {
+            long time0 = bytes.readVolatileLong(LAST_TIME);
+            long time0us = time0 / (HOST_IDS * 1000);
+            long time;
+            if (time0us >= timeus)
+                time = (time0us + 1) * (HOST_IDS * 1000);
+            else
+                time = timeus * (HOST_IDS * 1000);
+            if (bytes.compareAndSwapLong(LAST_TIME, time0, time))
+                return time / 1000 + hostId;
+            Jvm.nanoPause();
+        }
+    }
+
+    /**
+     * Return a unique, monotonically increasing nanosecond timestamp where the lowest two digits of the nanoseconds is the hostId.
+     *
+     * @return the timestamps with hostId as a long
+     * @throws IllegalStateException if the underlying resources have been released/closed
+     */
+    @Override
+    public long currentTimeNanos() throws IllegalStateException {
+        long time = provider.currentTimeNanos();
+        long time0 = bytes.readVolatileLong(LAST_TIME);
+        long timeN = time - time % HOST_IDS + hostId;
+
+        if (timeN > time0 && bytes.compareAndSwapLong(LAST_TIME, time0, timeN))
+            return timeN;
+
+        return currentTimeNanosLoop();
+    }
+
+    private long currentTimeNanosLoop() {
+        while (true) {
+            long time0 = bytes.readVolatileLong(LAST_TIME);
+            long next = time0 - time0 % HOST_IDS + hostId;
+            if (next <= time0)
+                next += HOST_IDS;
+            if (bytes.compareAndSwapLong(LAST_TIME, time0, next))
+                return next;
+            Jvm.nanoPause();
+        }
+    }
+
+    @Override
+    protected void performClose() {
+        super.performClose();
+        bytes.release(this);
+        file.releaseLast();
+    }
+}
---
+++ src/test/java/net/openhft/chronicle/bytes/DistributedUniqueTimeProviderTest.java
@@ -0,0 +1,103 @@
+package net.openhft.chronicle.bytes;
+
+import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.core.time.LongTime;
+import net.openhft.chronicle.core.time.TimeProvider;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DistributedUniqueTimeProviderTest extends BytesTestCommon {
+
+    static volatile long blackHole;
+
+    @Test
+    public void currentTimeMicros() {
+        TimeProvider tp = DistributedUniqueTimeProvider.INSTANCE;
+        long last = 0;
+        for (int i = 0; i < 100_000; i++) {
+            long time = tp.currentTimeMicros();
+            assertTrue(time > last);
+            assertEquals(LongTime.toMicros(time), time);
+            last = time;
+        }
+    }
+
+    @Test
+    public void currentTimeMicrosPerf() {
+        TimeProvider tp = DistributedUniqueTimeProvider.INSTANCE;
+        long start = System.currentTimeMillis();
+        int count = 0;
+        do {
+            for (int i = 0; i < 1000; i++)
+                blackHole = tp.currentTimeMicros();
+            count += 1000;
+        } while (System.currentTimeMillis() < start + 500);
+        System.out.println("currentTimeMicrosPerf count/sec: " + count * 2);
+        assertTrue(count > 230_000 / 2); // half the speed of Rasberry Pi
+    }
+
+    @Test
+    public void currentTimeNanosPerf() {
+        TimeProvider tp = DistributedUniqueTimeProvider.INSTANCE;
+        long start = System.currentTimeMillis();
+        int count = 0;
+        do {
+            for (int i = 0; i < 1000; i++)
+                blackHole = tp.currentTimeNanos();
+            count += 1000;
+        } while (System.currentTimeMillis() < start + 500);
+        System.out.println("currentTimeNanosPerf count/sec: " + count * 2);
+        assertTrue(count > 320_000 / 2); // half the speed of Rasberry Pi
+    }
+
+    @Test
+    public void currentTimeNanos() {
+        TimeProvider tp = DistributedUniqueTimeProvider.INSTANCE;
+        long start = tp.currentTimeNanos();
+        long last = start;
+        int count = 0;
+        long runTime = Jvm.isArm() ? 3_000_000_000L : 500_000_000L;
+        for (; ; ) {
+            long now = tp.currentTimeNanos();
+            assertEquals(LongTime.toNanos(now), now);
+            if (now > start + runTime)
+                break;
+            // check the times are different after shifting by 5 bits.
+            assertTrue((now >>> 5) > (last >>> 5));
+            last = now;
+            count++;
+            if (count >= 10_000_000)
+                break;
+        }
+        System.out.printf("count: %,d%n", count);
+        assertTrue(count > 1_000_000);
+    }
+
+    @Test
+    public void concurrentTimeNanos() {
+        long start0 = System.nanoTime();
+        final int runTimeUS = 5_000_000;
+        final int threads = Jvm.isArm() ? 4 : 16;
+        final int stride = Jvm.isArm() ? 1 : threads;
+        IntStream.range(0, threads)
+                .parallel()
+                .forEach(i -> {
+                    try (DistributedUniqueTimeProvider tp = DistributedUniqueTimeProvider.forHostId(i)) {
+                        long last = 0;
+                        for (int j = 0; j < runTimeUS; j += stride) {
+                            long now = tp.currentTimeNanos();
+                            assertEquals(i, now % 100);
+                            assertTrue(now > last);
+                            last = now;
+                        }
+                    }
+                });
+        long time0 = System.nanoTime() - start0;
+        System.out.printf("Time: %,d ms%n", time0 / 1_000_000);
+        assertTrue(Jvm.isArm() || time0 < runTimeUS * 1000L);
+    }
+}
