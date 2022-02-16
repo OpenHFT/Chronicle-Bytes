6655b6c610a7b6009f84cde1f11c90ab8bfdde21
Rob Austin <rob.austin@boundedbuffer.co.uk>
2022-02-16 08:03:09 +0000

    clean up bytes

--- src/test/java/net/openhft/chronicle/bytes/StopBitTest.java
+++ src/test/java/net/openhft/chronicle/bytes/StopBitTest.java
@@ -21,25 +21,29 @@ public class StopBitTest extends BytesTestCommon {
             final Bytes<byte[]> expectedBytes = Bytes.from(expected);
 
             final Bytes<ByteBuffer> b = Bytes.elasticByteBuffer();
-
-            if (expectedBytes == null) {
-                b.writeStopBit(-1);
-            } else {
-                long offset = expectedBytes.readPosition();
-                long readRemaining = Math.min(b.writeRemaining(), expectedBytes.readLimit() - offset);
-                b.writeStopBit(readRemaining);
-                try {
-                    b.write(expectedBytes, offset, readRemaining);
-                } catch (BufferUnderflowException | IllegalArgumentException e) {
-                    throw new AssertionError(e);
+            try {
+                if (expectedBytes == null) {
+                    b.writeStopBit(-1);
+                } else {
+                    long offset = expectedBytes.readPosition();
+                    long readRemaining = Math.min(b.writeRemaining(), expectedBytes.readLimit() - offset);
+                    b.writeStopBit(readRemaining);
+                    try {
+                        b.write(expectedBytes, offset, readRemaining);
+                    } catch (BufferUnderflowException | IllegalArgumentException e) {
+                        throw new AssertionError(e);
+                    }
                 }
+
+                // System.out.printf("0x%04x : %02x %02x %02x%n", i, b.readByte(0), b.readByte(1), b.readByte(3));
+
+                Assert.assertEquals("failed at " + i, expected, b.read8bit());
+
+            } finally {
+                b.releaseLast();
+                expectedBytes.releaseLast();
             }
-
-            // System.out.printf("0x%04x : %02x %02x %02x%n", i, b.readByte(0), b.readByte(1), b.readByte(3));
-
-            Assert.assertEquals("failed at " + i, expected, b.read8bit());
         }
-
     }
 
     @Test
@@ -52,22 +56,25 @@ public class StopBitTest extends BytesTestCommon {
         final Bytes<byte[]> bytes = Bytes.from(s);
 
         final Bytes<ByteBuffer> b = Bytes.elasticByteBuffer();
-
-        if (bytes == null) {
-            b.writeStopBit(-1);
-        } else {
-            long offset = bytes.readPosition();
-            long readRemaining = Math.min(b.writeRemaining(), bytes.readLimit() - offset);
-            b.writeStopBit(readRemaining);
-            try {
-                b.write(bytes, offset, readRemaining);
-            } catch (BufferUnderflowException | IllegalArgumentException e) {
-                throw new AssertionError(e);
+        try {
+            if (bytes == null) {
+                b.writeStopBit(-1);
+            } else {
+                long offset = bytes.readPosition();
+                long readRemaining = Math.min(b.writeRemaining(), bytes.readLimit() - offset);
+                b.writeStopBit(readRemaining);
+                try {
+                    b.write(bytes, offset, readRemaining);
+                } catch (BufferUnderflowException | IllegalArgumentException e) {
+                    throw new AssertionError(e);
+                }
             }
+
+            Assert.assertEquals(s, b.read8bit());
+        } finally {
+            bytes.releaseLast();
+            b.releaseLast();
         }
-
-        Assert.assertEquals(s, b.read8bit());
-
     }
 
 }
