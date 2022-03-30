7b380b8462673f285c1022612588fca8a94cc0a8
Ilya Kaznacheev <ilya.kaznacheev@chronicle.software>
2022-03-30 19:19:02 +0300

    Toggle zero cost assertions via zero.cost.assertions={disabled|enabled} property

--- src/test/java/net/openhft/chronicle/bytes/ZeroCostAssertionStatusTest.java
+++ src/test/java/net/openhft/chronicle/bytes/ZeroCostAssertionStatusTest.java
@@ -14,8 +14,15 @@ class ZeroCostAssertionStatusTest {
             ae = true;
         }
 
+        boolean zcae = false;
+        try {
+            assert AssertUtil.SKIP_ASSERTIONS || 0 != 0;
+        } catch (AssertionError assertionError) {
+            zcae = true;
+        }
+
         System.out.println("Normal assertions are " + (ae ? "ON" : "OFF"));
-        System.out.println("Zero-cost assertions are " + (AssertUtil.SKIP_ASSERTIONS ? "OFF" : "ON"));
+        System.out.println("Zero-cost assertions are " + (zcae ? "ON" : "OFF"));
     }
 
 }
