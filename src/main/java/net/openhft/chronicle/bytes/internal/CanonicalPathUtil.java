695e42238bbde41cfee0ebdd8069836ccad814d3
Jerry Shea <jerry.shea@chronicle.software>
2022-09-23 08:02:09 +1000

    remove prefix from CanonicalPathUtil so we can safely synchronize on a file across classloaders

--- src/main/java/net/openhft/chronicle/bytes/internal/CanonicalPathUtil.java
+++ src/main/java/net/openhft/chronicle/bytes/internal/CanonicalPathUtil.java
@@ -22,25 +22,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.SecureRandom;
 
 public final class CanonicalPathUtil {
-
-    // This is unlikely to collide with other internalized strings and is seldom the same across JVMs
-    private static final String PREFIX = CanonicalPathUtil.class.getName() + Math.abs(new SecureRandom().nextInt(1 << 30)) + "::";
 
     private CanonicalPathUtil() {}
 
     /**
-     * Returns an internalized String that is unlikely to collide with other internalized strings in the JVM.
+     * Returns an internalized String that can be used to synchronize on across classloaders
      *
      * @param file to use
      * @return internalized String
      */
     public static String of(@NotNull final File file) {
         try {
-            return (PREFIX + file.getCanonicalPath())
-                    .intern();
+            return file.getCanonicalPath().intern();
         } catch (IOException ioe) {
             throw new IORuntimeException("Unable to obtain the canonical path for " + file.getAbsolutePath(), ioe);
         }
