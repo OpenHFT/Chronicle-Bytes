139b7d67ed066f59572f3fff60a0d74088ec6359
Jerry Shea <jerry.shea@chronicle.software>
2022-09-23 08:26:15 +1000

    Ensure canonical path is not randomized so it can be used to synchronize on across classloaders. Fixes #443
    Co-authored-by: Rob Austin <rob.austin@chronicle.software>

--- src/main/java/net/openhft/chronicle/bytes/internal/CanonicalPathUtil.java
+++ src/main/java/net/openhft/chronicle/bytes/internal/CanonicalPathUtil.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016-2022 chronicle.software
+ *
+ *     https://chronicle.software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.openhft.chronicle.bytes.internal;
 
 import net.openhft.chronicle.core.io.IORuntimeException;
@@ -5,25 +22,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.SecureRandom;
 
 public final class CanonicalPathUtil {
-
-    // This is unlikely to collide with other internalized strings and is seldom the same across JVMs
-    private static final String PREFIX = CanonicalPathUtil.class.getName() + Math.abs(new SecureRandom().nextInt(1 << 30)) + "::";
 
     private CanonicalPathUtil() {}
 
     /**
-     * Returns an internalized String that is unlikely to collide with other internalized strings in the JVM.
+     * Returns an internalized String that can be used to synchronize on across classloaders
      *
      * @param file to use
      * @return internalized String
      */
     public static String of(@NotNull final File file) {
         try {
-            return (PREFIX + file.getCanonicalPath())
-                    .intern();
+            return file.getCanonicalPath().intern();
         } catch (IOException ioe) {
             throw new IORuntimeException("Unable to obtain the canonical path for " + file.getAbsolutePath(), ioe);
         }
