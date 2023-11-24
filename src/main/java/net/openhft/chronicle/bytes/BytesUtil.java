a09d54e8675fa2b4c8743ee6d59bd4f55a757704
chroniclekevinpowe <kevin.powe@chronicle.software>
2023-11-24 14:55:04 +1100

    Deprecated appendBytesFromStart method as part of removing its use from Chronicle FIX.

--- src/main/java/net/openhft/chronicle/bytes/BytesUtil.java
+++ src/main/java/net/openhft/chronicle/bytes/BytesUtil.java
@@ -518,7 +518,7 @@ public enum BytesUtil {
      * @throws ClosedIllegalStateException    If the resource has been released or closed.
      * @throws ThreadingIllegalStateException If this resource was accessed by multiple threads in an unsafe way.
      */
-    // used by Chronicle FIX.
+    @Deprecated(/* to be removed in x.27 */)
     public static void appendBytesFromStart(@NotNull Bytes<?> bytes, @NonNegative long startPosition, @NotNull StringBuilder sb)
             throws IllegalStateException {
         try {
