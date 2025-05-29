a50cfe14443b91198690c45a5a35bc296cc956d4
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-29 15:32:45 +0100

    Improve render package docs

--- src/main/java/net/openhft/chronicle/bytes/render/StandardDecimaliser.java
+++ src/main/java/net/openhft/chronicle/bytes/render/StandardDecimaliser.java
@@ -16,12 +16,8 @@
 package net.openhft.chronicle.bytes.render;
 
 /**
- * Implementation of {@link Decimaliser} that converts floating-point numbers to their decimal representation in
- * standard form. This class ensures that large numbers are represented in full, while small values are rounded
- * to a maximum of 18 decimal places.
- * <p>
- * Note: The conversion process first tries to utilize the {@link MaximumPrecision} with a precision of 18,
- * and if unsuccessful, falls back to a {@link UsesBigDecimal} strategy.
+ * Default decimaliser that attempts {@link MaximumPrecision} with precision 18
+ * and falls back to {@link UsesBigDecimal} for large numbers.
  */
 public class StandardDecimaliser implements Decimaliser {
 
@@ -31,18 +27,12 @@ public class StandardDecimaliser implements Decimaliser {
     public static final StandardDecimaliser STANDARD = new StandardDecimaliser();
 
     /**
-     * Instance of {@link MaximumPrecision} with a precision of 18, used for the initial attempt at conversion.
+     * Initial strategy rounding to eighteen decimal places.
      */
     static final MaximumPrecision PRECISION_18 = new MaximumPrecision(18);
 
     /**
-     * Converts a double value to its decimal representation in standard form and appends it using the provided
-     * {@link DecimalAppender}. The conversion process will round small values to a maximum of 18 decimal places,
-     * while large numbers will be represented in full.
-     *
-     * @param value           the double value to be converted.
-     * @param decimalAppender the {@link DecimalAppender} used to store and append the converted decimal value.
-     * @return true if the conversion and appending were successful; false otherwise.
+     * Convert {@code value} using {@link #PRECISION_18} then {@link UsesBigDecimal}.
      */
     @Override
     public boolean toDecimal(double value, DecimalAppender decimalAppender) {
@@ -52,13 +42,7 @@ public class StandardDecimaliser implements Decimaliser {
     }
 
     /**
-     * Converts a float value to its decimal representation in standard form and appends it using the provided
-     * {@link DecimalAppender}. The conversion process will round small values to a maximum of 18 decimal places,
-     * while large numbers will be represented in full.
-     *
-     * @param value           the float value to be converted.
-     * @param decimalAppender the {@link DecimalAppender} used to store and append the converted decimal value.
-     * @return true if the conversion and appending were successful; false otherwise.
+     * Convert {@code value} using {@link #PRECISION_18} then {@link UsesBigDecimal}.
      */
     @Override
     public boolean toDecimal(float value, DecimalAppender decimalAppender) {
