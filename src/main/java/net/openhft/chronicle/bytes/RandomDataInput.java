5fd9675f3c3cc5d21d79e2e02547c60001f6165c
Peter Lawrey <peter.lawrey@chronicle.software>
2025-06-04 09:27:27 +0100

    Hardening bounds checks for UTF-8 reads (#683)
    * Add bounds checks for readUtf8 methods
    
    * Update expected string length in NativeBytesStoreTest for accuracy
    
    * Refactor RandomDataInput to use offset directly for byte reading and buffer checks

--- src/main/java/net/openhft/chronicle/bytes/RandomDataInput.java
+++ src/main/java/net/openhft/chronicle/bytes/RandomDataInput.java
@@ -487,7 +487,7 @@ public interface RandomDataInput extends RandomCommon {
      * {@code null}
      * @throws IORuntimeException       If the reading operation encounters an unexpected error.
      * @throws IllegalArgumentException If the buffer is not a {@code StringBuilder} or {@code Bytes}.
-     * @throws BufferUnderflowException If the reading operation encounters the end of the byte source.
+     * @throws BufferUnderflowException If the offset or encoded length exceed {@code readLimit()}.
      * @throws ArithmeticException      If the calculated length of the UTF-8 encoded string is invalid.
      * @throws ClosedIllegalStateException    If the resource has been released or closed.
      * @throws ThreadingIllegalStateException If this resource was accessed by multiple threads in an unsafe way
@@ -496,7 +496,11 @@ public interface RandomDataInput extends RandomCommon {
     default <T extends Appendable & CharSequence> long readUtf8(@NonNegative long offset, @NotNull T sb)
             throws IORuntimeException, IllegalArgumentException, BufferUnderflowException, ArithmeticException, ClosedIllegalStateException {
         AppendableUtil.setLength(sb, 0);
-        // TODO insert some bounds check here
+
+        requireNonNegative(offset);
+        long remaining = requireNonNegative(readLimit() - offset);
+        if (remaining < 1)
+            throw new BufferUnderflowException();
 
         long utfLen;
         if ((utfLen = readByte(offset++)) < 0) {
@@ -522,6 +526,8 @@ public interface RandomDataInput extends RandomCommon {
         if (utfLen == -1)
             return ~offset;
         int len = Maths.toUInt31(utfLen);
+        if (requireNonNegative(readLimit() - offset) < len)
+            throw new BufferUnderflowException();
         BytesInternal.parseUtf8(this, offset, sb, true, len);
         return offset + utfLen;
     }
@@ -543,6 +549,7 @@ public interface RandomDataInput extends RandomCommon {
      * {@code null}
      * @throws ClosedIllegalStateException    If the resource has been released or closed.
      * @throws ThreadingIllegalStateException If this resource was accessed by multiple threads in an unsafe way
+     * @throws BufferUnderflowException       If the offset or encoded length exceed {@code readLimit()}.
      * @see RandomDataOutput#writeUtf8Limited(long, CharSequence, int)
      */
     default <T extends Appendable & CharSequence> long readUtf8Limited(@NonNegative long offset,
@@ -551,7 +558,12 @@ public interface RandomDataInput extends RandomCommon {
             throws IORuntimeException, IllegalArgumentException, BufferUnderflowException,
             ClosedIllegalStateException {
         AppendableUtil.setLength(sb, 0);
-        // TODO insert some bounds check here
+
+        requireNonNegative(offset);
+        requireNonNegative(maxUtf8Len);
+        long remaining = requireNonNegative(readLimit() - offset);
+        if (remaining < 1)
+            throw new BufferUnderflowException();
 
         long utfLen;
         if ((utfLen = readByte(offset++)) < 0) {
@@ -579,6 +591,8 @@ public interface RandomDataInput extends RandomCommon {
         if (utfLen > maxUtf8Len)
             throw new ClosedIllegalStateException("Attempted to read a char sequence of " +
                     "utf8 size " + utfLen + ", when only " + maxUtf8Len + " allowed");
+        if (requireNonNegative(readLimit() - offset) < utfLen)
+            throw new BufferUnderflowException();
         BytesInternal.parseUtf8(this, offset, sb, true, (int) utfLen);
         return offset + utfLen;
     }
