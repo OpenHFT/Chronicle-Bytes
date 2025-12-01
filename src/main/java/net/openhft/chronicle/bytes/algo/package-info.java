5d6a1c4b2c9837bad57f145ad5cb4c0494f8eccf
Peter Lawrey <peter.lawrey@chronicle.software>
2025-12-01 14:22:32 +0000

    Enhance documentation and clean up code in BytesStore and related classes

--- src/main/java/net/openhft/chronicle/bytes/algo/package-info.java
+++ src/main/java/net/openhft/chronicle/bytes/algo/package-info.java
@@ -1,4 +1,10 @@
 /*
  * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
  */
+/**
+ * Algorithms that operate on Chronicle {@code Bytes}.
+ * <p>
+ * Utilities in this package provide hashing, search and other
+ * low-level algorithms tuned for Chronicle byte stores.
+ */
 package net.openhft.chronicle.bytes.algo;
