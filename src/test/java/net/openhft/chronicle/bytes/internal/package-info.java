f6e89c3295ad253561a98faa2fbad0192bf984b4
Peter Lawrey <peter.lawrey@chronicle.software>
2025-12-01 15:04:31 +0000

    Add package-info documentation for Chronicle Bytes test packages

---
+++ src/test/java/net/openhft/chronicle/bytes/internal/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * Tests for internal Chronicle Bytes components.
+ */
+package net.openhft.chronicle.bytes.internal;
